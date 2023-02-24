b6f16115caf829bc86bb1864b583743e70334e14
Adityasusrname <adityasmailbooxx@gmail.com>
2023-02-24 18:54:00 +0530

    Created project with Spring Initializr

---
+++ src/main/java/com/adityasrivastava/taskmanagerwithauth/TaskmanagerwithauthApplication.java
@@ -0,0 +1,13 @@
+package com.adityasrivastava.taskmanagerwithauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagerwithauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanagerwithauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adityasrivastava/taskmanagerwithauth/TaskmanagerwithauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adityasrivastava.taskmanagerwithauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerwithauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
